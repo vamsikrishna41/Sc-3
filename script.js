ac05ad6bc3f679d48f58baf4dd4c2b3f43495b7b
Bathula Vamsi Krishna <121428816+vamsikrishna41@users.noreply.github.com>
2023-07-19 16:01:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function whatsapp(){
+     document.getElementById("whatsapp").src="https://w7.pngwing.com/pngs/194/809/png-transparent-logo-whatsapp-watercolor-splash.png";
+}
+function facebook(){
+    document.getElementById("whatsapp").src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png";
+}
+function instagram(){
+    document.getElementById("whatsapp").src="https://img.freepik.com/premium-vector/purple-gradiend-social-media-logo_197792-1883.jpg?w=2000";
+}
